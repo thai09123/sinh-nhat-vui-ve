eb36542bf78129516cd60970a48dabe012fa4968
thai09123 <109845812+thai09123@users.noreply.github.com>
2022-07-23 19:13:29 +0700

    Add files via upload
    create card

---
+++ present.js
@@ -0,0 +1,46 @@
+
+var to = 'Huyền!';
+var gift_url = 'https://d872003.github.io/HappyBirthdayHuyen/?fbclid=IwAR1t0zjCX70JSc2XZUsbUIsiOMiGkhmrbrCfyvpH-L2FFjdD6nJj4-Z7WFY';
+var gift_image_url = 'huyen.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
